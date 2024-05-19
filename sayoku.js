656e9d07f695470b256a114f33ba8059929ce998
Nqv4 <109492317+NQV4@users.noreply.github.com>
2024-05-19 18:58:05 +0900

    Add files via upload

---
+++ sayoku.js
@@ -0,0 +1,12 @@
+document.getElementById("button1").addEventListener("click", function() {
+    document.getElementById("message-container").innerText = "あなたは左翼です。〇ね";
+  });
+  
+  document.getElementById("button2").addEventListener("click", function() {
+    document.getElementById("message-container").innerText = "あなたは左翼です。〇ね";
+  });
+  
+  document.getElementById("button3").addEventListener("click", function() {
+    document.getElementById("message-container").innerText = "あなたは左翼です。〇ね";
+  });
+  
